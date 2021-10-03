13f8b9da7fa022da3e270cc435cf4e3c51f898fe
Someone Else Was Taken <cbtmail988@gmail.com>
2021-10-03 00:46:57 -0700

    Migrated Hydrogen

---
+++ Hydrogen Reforged/src/main/java/me/jellysquid/mods/hydrogen/client/resource/ModelCacheReloadListener.java
@@ -0,0 +1,3 @@
+package me.jellysquid.mods.hydrogen.client.resource;
+
+
---
+++ Hydrogen Reforged/src/main/java/me/jellysquid/mods/hydrogen/common/HydrogenMod.java
@@ -0,0 +1,16 @@
+package me.jellysquid.mods.hydrogen.common;
+
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+@Mod("hydrogen")
+public class HydrogenMod {
+    public static final Logger LOGGER = LogManager.getLogger("Hydrogen");
+
+    private static String MOD_VERSION;
+    public void setup(final FMLCommonSetupEvent event) {
+        MOD_VERSION = ModList.get().getModContainerById("sodium").get().getModInfo().getVersion().toString();
+    }
+}
