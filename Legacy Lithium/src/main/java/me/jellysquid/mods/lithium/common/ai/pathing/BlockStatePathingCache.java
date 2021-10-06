cb4a5a9507a90d02a6059f6ef7892abe88a6cc5e
Someone Else Was Taken <cbtmail988@gmail.com>
2021-10-06 02:13:49 -0700

    Misc Lithium Port Changes

--- Legacy Lithium/src/main/java/me/jellysquid/mods/lithium/common/ai/pathing/BlockStatePathingCache.java
+++ Legacy Lithium/src/main/java/me/jellysquid/mods/lithium/common/ai/pathing/BlockStatePathingCache.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.lithium.common.ai.pathing;
 
-import net.minecraft.entity.ai.pathing.PathNodeType;
+//import net.minecraft.entity.ai.pathing.PathNodeType;
+import net.minecraft.pathfinding.PathNodeType;
 
 public interface BlockStatePathingCache {
     PathNodeType getPathNodeType();
