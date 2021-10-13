4f88c5b99b6faf242f8270f118328204c6f96433
Someone Else Was Taken <justsomeoneelse91@gmail.com>
2021-10-13 00:57:18 -0700

    Implemented v1.3 Mod Overrides

--- Legacy Sodium/src/main/java/me/jellysquid/mods/sodium/common/config/SodiumConfig.java
+++ Legacy Sodium/src/main/java/me/jellysquid/mods/sodium/common/config/SodiumConfig.java
@@ -4,6 +4,7 @@ package me.jellysquid.mods.sodium.common.config;
 //import net.fabricmc.loader.api.ModContainer;
 //import net.fabricmc.loader.api.metadata.CustomValue;
 //import net.fabricmc.loader.api.metadata.ModMetadata;
+import net.minecraftforge.fml.loading.FMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -94,52 +95,24 @@ public class SodiumConfig {
         }
     }
 
-    /*
+
     private void applyModOverrides() {
-        for (ModContainer container : FabricLoader.getInstance().getAllMods()) {
-            ModMetadata meta = container.getMetadata();
+        if(FMLLoader.getLoadingModList().getModFileById("quark") != null)
+        {
+            this.options.get("mixin.features.item").addModOverride(false, "quark");
+        }
 
-            if (meta.containsCustomValue(JSON_KEY_SODIUM_OPTIONS)) {
-                CustomValue overrides = meta.getCustomValue(JSON_KEY_SODIUM_OPTIONS);
+        if(FMLLoader.getLoadingModList().getModFileById("tetra") != null)
+        {
+            this.options.get("mixin.features.item").addModOverride(false, "tetra");
+        }
 
-                if (overrides.getType() != CustomValue.CvType.OBJECT) {
-                    LOGGER.warn("Mod '{}' contains invalid Sodium option overrides, ignoring", meta.getId());
-                    continue;
-                }
-
-                for (Map.Entry<String, CustomValue> entry : overrides.getAsObject()) {
-                    this.applyModOverride(meta, entry.getKey(), entry.getValue());
-                }
-            }
+        if(FMLLoader.getLoadingModList().getModFileById("abnormals_core") != null)
+        {
+            this.options.get("mixin.features.world_ticking").addModOverride(false, "abnormals_core");
         }
     }
 
-    private void applyModOverride(ModMetadata meta, String name, CustomValue value) {
-        Option option = this.options.get(name);
-
-        if (option == null) {
-            LOGGER.warn("Mod '{}' attempted to override option '{}', which doesn't exist, ignoring", meta.getId(), name);
-            return;
-        }
-
-        if (value.getType() != CustomValue.CvType.BOOLEAN) {
-            LOGGER.warn("Mod '{}' attempted to override option '{}' with an invalid value, ignoring", meta.getId(), name);
-            return;
-        }
-
-        boolean enabled = value.getAsBoolean();
-
-        // disabling the option takes precedence over enabling
-        if (!enabled && option.isEnabled()) {
-            option.clearModsDefiningValue();
-        }
-
-        if (!enabled || option.isEnabled() || option.getDefiningMods().isEmpty()) {
-            option.addModOverride(enabled, meta.getId());
-        }
-    }
-
-     */
 
     /**
      * Returns the effective option for the specified class name. This traverses the package path of the given mixin
@@ -199,7 +172,7 @@ public class SodiumConfig {
 
         SodiumConfig config = new SodiumConfig();
         config.readProperties(props);
-        //config.applyModOverrides();
+        config.applyModOverrides();
 
         return config;
     }
