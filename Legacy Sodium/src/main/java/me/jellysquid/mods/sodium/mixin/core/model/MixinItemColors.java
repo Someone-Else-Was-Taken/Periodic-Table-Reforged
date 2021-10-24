b9d59dfec6053cf5fda3c05378557fb79be4b065
evanbones <bownessevan@gmail.com>
2021-10-23 19:37:52 -0700

    Fix Tetra and Tinker's tools not rendering properly

--- Legacy Sodium/src/main/java/me/jellysquid/mods/sodium/mixin/core/model/MixinItemColors.java
+++ Legacy Sodium/src/main/java/me/jellysquid/mods/sodium/mixin/core/model/MixinItemColors.java
@@ -7,9 +7,11 @@ import me.jellysquid.mods.sodium.client.world.biome.ItemColorsExtended;
 //import net.minecraft.client.color.item.ItemColors;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.item.Item;
 //import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IItemProvider;
+import net.minecraftforge.registries.IRegistryDelegate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemColors.class)
 public class MixinItemColors implements ItemColorsExtended {
-    private Reference2ReferenceMap<IItemProvider, IItemColor> itemsToColor;
+    private Reference2ReferenceMap<IRegistryDelegate<Item>, IItemColor> itemsToColor;
 
     private static final IItemColor DEFAULT_PROVIDER = (stack, tintIdx) -> -1;
 
@@ -30,12 +32,12 @@ public class MixinItemColors implements ItemColorsExtended {
     @Inject(method = "register", at = @At("HEAD"))
     private void preRegisterColor(IItemColor mapper, IItemProvider[] convertibles, CallbackInfo ci) {
         for (IItemProvider convertible : convertibles) {
-            this.itemsToColor.put(convertible.asItem(), mapper);
+            this.itemsToColor.put(convertible.asItem().delegate, mapper);
         }
     }
 
     @Override
     public IItemColor getColorProvider(ItemStack stack) {
-        return this.itemsToColor.get(stack.getItem());
+        return this.itemsToColor.get(stack.getItem().delegate);
     }
 }
