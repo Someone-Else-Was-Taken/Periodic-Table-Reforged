770f4130dd2522fa9ec6489ff382156d08e81e64
Someone Else Was Taken <justsomeoneelse91@gmail.com>
2021-11-11 19:31:46 -0800

    Initial Hydrogen Port: Take 2

---
+++ Legacy Hydrogen/src/main/java/me/jellysquid/mods/hydrogen/client/HydrogenClientPreLaunch.java
@@ -0,0 +1,16 @@
+package me.jellysquid.mods.hydrogen.client;
+
+//import me.jellysquid.mods.hydrogen.client.resource.ModelCacheReloadListener;
+import net.minecraft.resources.ResourcePackType;
+//import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+//import net.minecraft.resource.ResourceType;
+
+public class HydrogenClientPreLaunch {
+    public static void init() {
+        /*
+        ResourceManagerHelper.get(ResourcePackType.CLIENT_RESOURCES)
+                .registerReloadListener(new ModelCacheReloadListener());
+
+         */
+    }
+}
