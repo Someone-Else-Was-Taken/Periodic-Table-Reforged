5caf7cb779561682ded6d9f0ee3eac35dfdefda1
Someone Else Was Taken <justsomeoneelse91@gmail.com>
2021-11-13 20:05:15 -0800

    Iris Port

---
+++ LegacyIris/src/main/java/net/coderbot/iris/mixin/DimensionTypeAccessor.java
@@ -0,0 +1,16 @@
+package net.coderbot.iris.mixin;
+
+import net.minecraft.world.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.OptionalLong;
+//import net.minecraft.world.level.dimension.DimensionType;
+
+@Mixin(DimensionType.class)
+public interface DimensionTypeAccessor {
+
+	@Accessor
+	OptionalLong getFixedTime();
+
+}
