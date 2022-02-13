db40f7de0ae39bbca90c7da652319f5333d5460e
LZ洛杰 <58633604+Molarczsq@users.noreply.github.com>
2022-02-13 20:21:18 +0800

    Delete Flower Sodium directory

--- Flower Sodium/src/main/java/me/jellysquid/mods/sodium/client/model/ModelCuboidAccessor.java
+++
@@ -1,7 +0,0 @@
-package me.jellysquid.mods.sodium.client.model;
-
-import net.minecraft.client.renderer.model.ModelRenderer;
-
-public interface ModelCuboidAccessor {
-    ModelRenderer.TexturedQuad[] getQuads();
-}
