e3a7461bd2cdb1d8cee43ef57a5eed29107e5b5e
LZ洛杰 <58633604+Molarczsq@users.noreply.github.com>
2022-02-13 20:22:13 +0800

    Delete Legacy Sodium directory

--- Legacy Sodium/src/main/java/me/jellysquid/mods/sodium/client/world/biome/BlockColorsExtended.java
+++
@@ -1,9 +0,0 @@
-package me.jellysquid.mods.sodium.client.world.biome;
-
-import net.minecraft.block.BlockState;
-//import net.minecraft.client.color.block.BlockColorProvider;
-import net.minecraft.client.renderer.color.IBlockColor;
-
-public interface BlockColorsExtended {
-    IBlockColor getColorProvider(BlockState state);
-}
--- Legacy Sodium/src/main/java/me/jellysquid/mods/sodium/client/world/biome/ItemColorsExtended.java
+++
@@ -1,9 +0,0 @@
-package me.jellysquid.mods.sodium.client.world.biome;
-
-//import net.minecraft.client.color.item.ItemColorProvider;
-import net.minecraft.client.renderer.color.IItemColor;
-import net.minecraft.item.ItemStack;
-
-public interface ItemColorsExtended {
-    IItemColor getColorProvider(ItemStack stack);
-}
