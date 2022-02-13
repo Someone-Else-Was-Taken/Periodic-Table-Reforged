0f3a86eb0233c01294dffd2ef1a25ec48598aa76
LZ洛杰 <58633604+Molarczsq@users.noreply.github.com>
2022-02-13 20:21:52 +0800

    Delete Legacy Phosphor directory

--- Legacy Phosphor/src/main/java/me/jellysquid/mods/phosphor/common/chunk/light/SharedBlockLightData.java
+++
@@ -1,8 +0,0 @@
-package me.jellysquid.mods.phosphor.common.chunk.light;
-
-public interface SharedBlockLightData {
-    /**
-     * Marks this instance as a shared copy which cannot be directly written into.
-     */
-    void makeSharedCopy();
-}
